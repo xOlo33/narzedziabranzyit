d87e352442125d05deb4d6f1abe84516b36fac9a
xOlo33 <oliwerlomnicki1@gmail.com>
2024-05-22 13:49:11 +0200

    Yaml

--- projekt.py
+++ projekt.py
@@ -62,3 +62,21 @@ def load_yaml(file_path):
     else:
         print("Unsupported input format")
         sys.exit(1)
+if _name_ == "_main_":
+    input_file, output_file = parse_arguments()
+
+    if input_file.endswith('.json'):
+        data = load_json(input_file)
+    elif input_file.endswith('.yml') or input_file.endswith('.yaml'):
+        data = load_yaml(input_file)
+    else:
+        print("Unsupported input format")
+        sys.exit(1)
+
+    if output_file.endswith('.json'):
+        save_json(data, output_file)
+    elif output_file.endswith('.yml') or output_file.endswith('.yaml'):
+        save_yaml(data, output_file)
+    else:
+        print("Unsupported output format")
+        sys.exit(1)
